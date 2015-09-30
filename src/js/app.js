c09f95b3ba39ce98bec1812e7d586ac188ead14f
minyu007 <kevin.minyu@gmail.com>
2015-09-30 20:56:24 +0800

    sync

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-define(['jquery', 'fastclick'], function($, fastclick){
+define(['jquery', 'fastclick'], function($, fastclick) {
 	fastclick.attach(document.body);
 
 	var Dom = {
@@ -7,51 +7,51 @@ define(['jquery', 'fastclick'], function($, fastclick){
 		body: $('body')
 	}
 
-	var app =  {
-		init: function(){
+	var app = {
+		init: function() {
 			this.toggleSearch();
 			this.toggleUser();
 			this.toggleNavMore();
 		},
 
-		toggleSearch: function(){
+		toggleSearch: function() {
 			var searchBtn = Dom.header.find('.searchBtn'),
 				closeBtn = Dom.header.find('.searchMobile .close');
-			searchBtn.on('click', function(){
+			searchBtn.on('click', function() {
 				Dom.body.addClass('mobileSearchOpen');
-				
+
 			});
-			closeBtn.on('click', function(){
+			closeBtn.on('click', function() {
 				Dom.body.removeClass('mobileSearchOpen');
 			});
 
 		},
 
-		toggleUser: function(){
+		toggleUser: function() {
 			var userBtn = Dom.header.find('.userBtn'),
 				closeBtn = Dom.header.find('.navContent .close');
-			userBtn.on('click', function(){
+			userBtn.on('click', function() {
 				Dom.body.addClass('mobileNavOpen');
-				
+
 			});
-			closeBtn.on('click', function(){
+			closeBtn.on('click', function() {
 				Dom.body.addClass('animationClose');
 
-				setTimeout(function(){
+				setTimeout(function() {
 					Dom.body.removeClass('animationClose')
 					Dom.body.removeClass('mobileNavOpen');
 					Dom.body.removeClass('mobileNavClose');
 				}, 250);
-				
+
 			});
 		},
-		toggleNavMore: function(){
+		toggleNavMore: function() {
 			var moreBtn = Dom.header.find('.primaryNav li.toggleInMobile');
-			moreBtn.on('click', function(){
-				if(Dom.body.hasClass('mobileNavMoreOpen')){
+			moreBtn.on('click', function() {
+				if (Dom.body.hasClass('mobileNavMoreOpen')) {
 					Dom.body.removeClass('mobileNavMoreOpen');
 					$(this).find('i').removeClass('fa-angle-double-up').addClass('fa-angle-double-down');
-				}else{
+				} else {
 					Dom.body.addClass('mobileNavMoreOpen');
 					$(this).find('i').removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
 				}
