8dd69821d1f8c1c040ba5da3d7e1f01b0db66d84
minyu007 <kevin.minyu@gmail.com>
2015-09-30 21:22:42 +0800

    sync

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
 require.config({
-	urlArgs: "v=0.1",
-	//baseUrl: 'scripts/',
-	paths: {
-	
-		jquery: '../../bower_components/jquery/dist/jquery',
-		fastclick: '../../bower_components/fastclick/lib/fastclick',
-	},
-	shim: {
-		
-	},
-	deps: [
-		'app'
-	]
+    urlArgs: "v=0.1",
+    //baseUrl: 'scripts/',
+    paths: {
+
+        jquery: '../../bower_components/jquery/dist/jquery',
+        fastclick: '../../bower_components/fastclick/lib/fastclick',
+    },
+    shim: {
+
+    },
+    deps: [
+        'app'
+    ]
 });
